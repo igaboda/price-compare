42b79ade27df6228b505fbdaadb81575c41d77d6
igaboda <bodalska.iga@gmail.com>
2022-10-19 14:13:23 +0200

    migrations added without simple_history

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simple_history',
+    'price',
+    'product',
+    'shop',
+    'user',
+    # 'simple_history',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware',
+    # 'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
