5405d2785cecadef7abd1599491c094afbff4410
igaboda <bodalska.iga@gmail.com>
2022-11-09 16:44:43 +0100

    changes to models columns; product_group added

--- app/shop/models.py
+++ app/shop/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+class ShopParserType(models.TextChoices):
+    SOUP = 'soup'
+    WEBDRIVER = 'webdriver'
+
+
 class Shop(models.Model):
     """Shop object"""
-    shop_name = models.CharField(max_length=255, null=False)
-    shop_url = models.CharField(max_length=255, null=False)
+    shop_name = models.CharField(max_length=255)
+    shop_url = models.CharField(max_length=255)
+    search_param = models.CharField(max_length=255)
+    parser_type = models.CharField(choices=ShopParserType.choices,
+                                   max_length=20)
 
