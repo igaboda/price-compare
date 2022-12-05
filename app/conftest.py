4d4b7546af83da5b18036afebb6e106ca456a08d
igaboda <bodalska.iga@gmail.com>
2022-12-05 11:12:16 +0100

    descriptions added to conftest.py

--- app/conftest.py
+++ app/conftest.py
@@ -12,11 +12,13 @@ DRIVER_PATH = '/usr/local/bin/chromedriver'
 
 @pytest.fixture
 def load_shops(db):
+    """Loads shops data to database."""
     call_command('loaddata', f'{DATA_PATH}shop_test_data.json')
 
 
 @pytest.fixture
 def initialize_parser(db):
+    """Factory function for initializing given ShopParser object."""
     def _initialize_parser(shop):
         parser_obj = get_shop_parser(shop)
         shop_data = Shop.objects.defer('shop_name').get(shop_name=shop)
